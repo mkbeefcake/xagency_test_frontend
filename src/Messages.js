843cc9e3fce4ce174c920d2b3de8886306086ddd
Mark <mkblockchaindev@outlook.com>
2024-09-19 15:59:22 +0000

    organized well for system message

--- src/Messages.js
+++ src/Messages.js
@@ -7,7 +7,9 @@ const SystemMessage = ({message}) => {
             <div className="flex items-end w-auto bg-gray-500 dark:bg-gray-800 m-1 rounded-xl rounded-br-none sm:w-3/4 md:w-auto">
                 <div className="p-2">
                     <div className="text-gray-200">
-                        {message?.replace("\n", "<br/>")}
+                        {message?.split('\n').map((line, index) => (
+                            <div key={index} class="pb-3">{line}</div>
+                        ))}
                     </div>
                 </div>
             </div>
