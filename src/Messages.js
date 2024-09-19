99e1f8ae95a0f873cd8d1b0ef304bd4164a132bb
Mark <mkblockchaindev@outlook.com>
2024-09-19 14:39:30 +0000

    simple user interaction

--- src/Messages.js
+++ src/Messages.js
@@ -1,17 +1,82 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const SystemMessage = ({message}) => {
+    return (
+        <div className="flex justify-end">
+            <div className="flex items-end w-auto bg-gray-500 dark:bg-gray-800 m-1 rounded-xl rounded-br-none sm:w-3/4 md:w-auto">
+                <div className="p-2">
+                    <div className="text-gray-200">
+                        {message}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )    
+}
+
+const UserMessage = ({message}) => {
+    return (
+        <div className="flex items-end w-3/4">
+            <img className="w-8 h-8 m-3 rounded-full" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="avatar"/>  
+            <div className="p-3 bg-gray-300 dark:bg-gray-800 mx-3 my-1 rounded-2xl rounded-bl-none sm:w-3/4 md:w-3/6">
+                <div className="text-gray-700 dark:text-gray-200">
+                    {message}
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const Messages = () => {
+
+    const [input, setInput] = useState("");
+    const [messages, setMessages] = useState([
+        {
+            message: "Hello I am ChatGPT",
+            sender: "ChatGPT"
+        }
+    ]);
+
+    const handleChange = (event)=>{
+        setInput(event.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && event.ctrlKey) {
+            handleSend(event)
+        }
+    }
+
+    const handleSend = async (event)=>{
+        const newMessage = {
+            message: input,
+            sender: "user"
+        }
+
+        const newMessages = [...messages,newMessage];
+        setMessages(newMessages);
+        setInput('');
+        await processMessageToChatGPT(newMessages);
+    }
+
+    async function processMessageToChatGPT(chatMessages){
+        setMessages(
+            [
+                ...chatMessages,
+                {
+                    message: "Hi there",
+                    sender: "ChatGPT"
+                }
+            ]
+        )
+    }    
+
     return (
         <div className="flex-grow h-full flex flex-col">
             <div className="w-full h-15 p-1 bg-gray-800 dark:bg-gray-900 shadow-md rounded-xl rounded-bl-none rounded-br-none">
                 <div className="flex p-2 align-middle items-center">
-                    <div className="p-2 md:hidden rounded-full mr-1 hover:bg-gray-600 text-white">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
-                        </svg>
-                    </div>
                     <div className="border rounded-full border-white p-1/2">
-                        <img className="w-14 h-14 rounded-full" src="https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png" alt="avatar"/>
+                        <img className="w-14 h-14 rounded-full" src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1760&q=80" alt="avatar"/>
                     </div>
                     <div className="flex-grow p-2">
                         <div className="text-md text-gray-50 font-semibold">Me </div>
@@ -25,32 +90,14 @@ const Messages = () => {
                 </div>
             </div>
             <div className="w-full flex-grow bg-gray-100 dark:bg-gray-900 my-2 p-2 overflow-y-auto">
-                <div className="flex items-end w-3/4" >
-                    <img className="hidden w-8 h-8 m-3 rounded-full" src="https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png" alt="avatar"/>  
-                    <div className="w-8 m-3 rounded-full"/>
-                    <div className="p-3 bg-gray-300 dark:bg-gray-800 mx-3 my-1 rounded-2xl rounded-bl-none sm:w-3/4 md:w-3/6">
-                        <div className="text-gray-700 dark:text-gray-200">
-                            Hello there
-                        </div>
-                    </div>
-                </div>
-                <div className="flex justify-end">
-                    <div className="flex items-end w-auto bg-gray-500 dark:bg-gray-800 m-1 rounded-xl rounded-br-none sm:w-3/4 md:w-auto">
-                        <div className="p-2">
-                            <div className="text-gray-200">
-                                Hello ? How Can i help you ?
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex items-end w-3/4">
-                    <img className="w-8 h-8 m-3 rounded-full" src="https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png" alt="avatar"/>  
-                    <div className="p-3 bg-gray-300 dark:bg-gray-800 mx-3 my-1 rounded-2xl rounded-bl-none sm:w-3/4 md:w-3/6">
-                        <div className="text-gray-700 dark:text-gray-200">
-                            Thanks
-                        </div>
-                    </div>
-                </div>
+                {messages.map((message, index) => {
+                    if (message.sender === "ChatGPT") {
+                        return (<SystemMessage message={message.message} />)
+                    }
+                    else {
+                        return (<UserMessage message={message.message} />)
+                    }
+                })}                
             </div>
             <div className="h-15  p-3 rounded-xl rounded-tr-none rounded-tl-none bg-gray-100 dark:bg-gray-800">
                 <div className="flex items-center">
@@ -60,10 +107,13 @@ const Messages = () => {
                         </svg>
                     </div>
                     <div className="search-chat flex flex-grow p-2">
-                        <input className="input text-gray-700 dark:text-gray-200 text-sm p-5 focus:outline-none bg-gray-100 dark:bg-gray-800  flex-grow rounded-l-md" type="text" placeholder="Type your message ..."/>
+                        <input className="input text-gray-700 dark:text-gray-200 text-sm p-5 focus:outline-none bg-gray-100 dark:bg-gray-800  flex-grow rounded-l-md" 
+                            type="text" placeholder="Type your message ..." value={input}
+                            onChange={handleChange} onKeyDown={handleKeyDown} 
+                        />
                         <div className="bg-gray-100 dark:bg-gray-800 dark:text-gray-200  flex justify-center items-center pr-3 text-gray-400 rounded-r-md">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
                             </svg>
                         </div>
                     </div>
