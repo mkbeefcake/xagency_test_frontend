9e473c2fdf0df2fe588c71394e42a6a6ff1919f9
Mark <mkblockchaindev@outlook.com>
2024-09-19 13:52:06 +0000

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([
+  ]);
+
+  const handleChange = (event)=>{
+      setInput(event.target.value)
+  }
+
+  const handleSend = async (event)=>{
+      event.preventDefault()
+      const newMessage = {
+          message: input,
+          sender: "user"
+      }
+
+      const newMessages = [...messages,newMessage];
+
+      setMessages(newMessages);
+      setInput('');
+      await processMessageToChatGPT(newMessages);
+  }
+
+  async function processMessageToChatGPT(chatMessages){
+    setMessages(
+        [
+            ...chatMessages,
+            {
+                message: "Hi ",
+                sender: "ChatGPT"
+            }
+        ]
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="response-area">
+        {messages.map((message, index) => {
+            return(
+                <div className={message.sender==="ChatGPT" ? 'gpt-message message' : 'user-message message'}>{message.message}</div>
+            );
+        })}
+      </div>
+      <div className="prompt-area">
+        <input type="text" placeholder="Send a message..." value={input} onChange={handleChange}/>
+        <button className="submit" type="submit" onClick={handleSend}>Send</button>
+      </div>
     </div>
   );
 }
